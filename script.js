53dc7421448e94e9d83de7da9c9ca60bebaf46a6
kenken0806 <kenichiro_suzuki@mail.toyota.co.jp>
2024-07-18 17:15:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 "use strict";
 
+const meetButtons = document.querySelectorAll(".meetPage");
+
+// meetButtons[0].addEventListener("click", () => window.location.reload());
+// meetButtons[1].addEventListener("click", () => window.location.reload());
+// meetButtons[2].addEventListener("click", () => window.location.reload());
 
 
+const prices0 = document.querySelectorAll(".price")[0];
+const prices1 = document.querySelectorAll(".price")[1];
+const prices2 = document.querySelectorAll(".price")[2];
 
-const prices0 = document.querySelectorAll(".price")[0]
-const prices1 = document.querySelectorAll(".price")[1]
-const prices2 = document.querySelectorAll(".price")[2]
-
-const grams0 = document.querySelectorAll(".pp")[0]
-const grams1 = document.querySelectorAll(".pp")[1]
-const grams2 = document.querySelectorAll(".pp")[2]
+const grams0 = document.querySelectorAll(".pp")[0];
+const grams1 = document.querySelectorAll(".pp")[1];
+const grams2 = document.querySelectorAll(".pp")[2];
 
 
 
 function changePrice0 (){
+    window.location.reload();
     prices0.innerHTML = grams0.value;
 }
 
