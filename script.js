fac3b10e6f1ecc2b1683bf96bca72985dbc271d7
kenken0806 <kenichiro_suzuki@mail.toyota.co.jp>
2024-07-18 16:25:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-'use strict'
-// ここに JavaScript のコードを書きましょう
+"use strict";
+
+
+
+
+const prices0 = document.querySelectorAll(".price")[0]
+const prices1 = document.querySelectorAll(".price")[1]
+const prices2 = document.querySelectorAll(".price")[2]
+
+const grams0 = document.querySelectorAll(".pp")[0]
+const grams1 = document.querySelectorAll(".pp")[1]
+const grams2 = document.querySelectorAll(".pp")[2]
+
+
+
+function changePrice0 (){
+    prices0.innerHTML = grams0.value;
+}
+
+
+grams0.addEventListener("change", changePrice0);
+
+function changePrice1 (){
+    prices1.innerHTML = grams1.value;
+}
+
+
+grams1.addEventListener("change", changePrice1);
+
+function changePrice2 (){
+    prices2.innerHTML = grams2.value;
+}
+
+
+grams2.addEventListener("change", changePrice2);
